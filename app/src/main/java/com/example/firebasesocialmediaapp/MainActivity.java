bc474ad67cc3eb7df3cc25ed503e5c42ad56dcb0
Arto <59399021+JustArto@users.noreply.github.com>
2021-05-31 15:17:03 +0400

    FireBaseApp
    Social Media App

---
+++ app/src/main/java/com/example/firebasesocialmediaapp/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.firebasesocialmediaapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText edtEmail, edtPassword;
+    private Button btnSignUp, btnSignIn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            try {
+                signUp();
+            }catch (Exception e){
+                Toast.makeText(MainActivity.this, e.toString(),Toast.LENGTH_LONG).show();
+            }
+
+
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    signIn();
+                }catch (Exception e) {
+                    Toast.makeText(MainActivity.this, e.toString(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+
+            // Transition to the next activity
+
+            Intent intent = new Intent(MainActivity.this, SocialMediaActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    private void signIn() {
+
+        mAuth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            Toast.makeText(MainActivity.this, "Authentication Successful.",
+                                    Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(MainActivity.this, SocialMediaActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+
+    }
+
+    private void signUp() {
+
+        mAuth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            // Add the user to the real time database
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("app_users").child(task.getResult()
+                                    .getUser().getUid()).child("email")
+                                    .setValue(task.getResult().getUser()
+                                            .getEmail());
+
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("app_users").child(task.getResult()
+                                    .getUser().getUid()).child("name")
+                                    .setValue("johnson");
+
+                            // Transition to the next activity
+
+                            Intent intent = new Intent(MainActivity.this, SocialMediaActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+}
